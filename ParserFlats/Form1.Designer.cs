cd7c3c98a8ab595806f610be2d0a8c8d5420c660
gilyazovigor <gilyazovigorperm@gmail.com>
2019-02-17 19:49:01 +0500

    бетта версия

--- ParserFlats/Form1.Designer.cs
+++ ParserFlats/Form1.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1_perm = new System.Windows.Forms.RadioButton();
+            this.radioButton2_ekat = new System.Windows.Forms.RadioButton();
+            this.radioButton3_nn = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть сайт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1_perm
+            // 
+            this.radioButton1_perm.AutoSize = true;
+            this.radioButton1_perm.Checked = true;
+            this.radioButton1_perm.Location = new System.Drawing.Point(59, 64);
+            this.radioButton1_perm.Name = "radioButton1_perm";
+            this.radioButton1_perm.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1_perm.TabIndex = 2;
+            this.radioButton1_perm.TabStop = true;
+            this.radioButton1_perm.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2_ekat
+            // 
+            this.radioButton2_ekat.AutoSize = true;
+            this.radioButton2_ekat.Location = new System.Drawing.Point(154, 64);
+            this.radioButton2_ekat.Name = "radioButton2_ekat";
+            this.radioButton2_ekat.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2_ekat.TabIndex = 3;
+            this.radioButton2_ekat.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3_nn
+            // 
+            this.radioButton3_nn.AutoSize = true;
+            this.radioButton3_nn.Location = new System.Drawing.Point(261, 64);
+            this.radioButton3_nn.Name = "radioButton3_nn";
+            this.radioButton3_nn.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3_nn.TabIndex = 4;
+            this.radioButton3_nn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Выберите город:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Пермь";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Екатеринбург";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Нижний Новгород";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1-10",
+            "11-20",
+            "21-30",
+            "31-40",
+            "41-50",
+            "51-60"});
+            this.comboBox1.Location = new System.Drawing.Point(47, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Страницы:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 42);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Собрать данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(366, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 227);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "Ссылки собранных объявлений:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(618, 306);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3_nn);
+            this.Controls.Add(this.radioButton2_ekat);
+            this.Controls.Add(this.radioButton1_perm);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Web scraper for CIAN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1_perm;
+        private System.Windows.Forms.RadioButton radioButton2_ekat;
+        private System.Windows.Forms.RadioButton radioButton3_nn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ParserFlats/Form1.cs
+++ ParserFlats/Form1.cs
@@ -7,14 +7,276 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+using System.Threading;
 
 namespace ParserFlats
 {
     public partial class Form1 : Form
     {
+        IWebDriver Browser;
+        string path = @"C:\курсовая\парсер недвижимость курсач\данные.xlsx";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        string cian_url_perm = "";
+
+        string cian_url_ekat = "";
+
+        string cian_url_nn = "https://" + "nn.cian.ru/cat.php?deal_type=sale&" +
+                "engine_version=2&house_material%5B0%5D=1&house_material%5B1%5D=2&" +
+                "house_material%5B2%5D=3&max_house_year=2050&maxfloor=1000&maxfloorn=1000&" +
+                "maxtarea=1000&min_house_year=1901&minfloor=0&minfloorn=0&mintarea=0&" +
+                "offer_type=flat&region=4885&room1=1&room2=1";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.ToString() == "")
+            {
+                MessageBox.Show("Выберите страницы");
+                return;
+            }
+
+            if (Browser == null)
+            {
+                string cian_url = "";
+                string cb_value = comboBox1.Text.ToString();
+                string url_page = cb_value.Substring(0, cb_value.Length - 3);
+
+                if (radioButton1_perm.Checked)
+                    cian_url = cian_url_perm + "&p=" + url_page;
+
+                if (radioButton2_ekat.Checked)
+                    cian_url = cian_url_ekat + "&p=" + url_page;
+
+                if (radioButton3_nn.Checked)
+                    cian_url = cian_url_nn + "&p=" + url_page;
+
+                Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+                Browser.Manage().Window.Maximize();
+                Browser.Navigate().GoToUrl(cian_url);
+                comboBox1.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Браузер уже открыт");
+                return;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.ToString() == "")
+            {
+                MessageBox.Show("Выберите страницы");
+                return;
+            }
+
+            if (Browser == null)
+            {
+                MessageBox.Show("Откройте браузер");
+                return;
+            }
+
+            //Browser.Quit();
+            //.c6e8ba5398-title--3WDDX
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[1];
+            ws.Cells[1, 1].Value2 = "х1 (общ пл)";
+            ws.Cells[1, 2].Value2 = "х2 (комнат)";
+            ws.Cells[1, 3].Value2 = "х3 (этаж)";
+            ws.Cells[1, 4].Value2 = "х4 (кол этажей)";
+            ws.Cells[1, 5].Value2 = "х5 (долгота)";
+            ws.Cells[1, 6].Value2 = "х6 (широта)";
+            ws.Cells[1, 7].Value2 = "х7 (год постройки дома)";
+            ws.Cells[1, 8].Value2 = "х8 (тип стен)";
+            ws.Cells[1, 9].Value2 = "х9 (ставка реф)";
+            ws.Cells[1, 10].Value2 = "х10 (ср зп)";
+            ws.Cells[1, 11].Value2 = "х11 (ВВП)";
+            ws.Cells[1, 12].Value2 = "х12 (РТС)";
+            ws.Cells[1, 13].Value2 = "х13 (курс доллара)";
+            ws.Cells[1, 14].Value2 = "х14 (цена нефти)";
+            ws.Cells[1, 15].Value2 = "х15 (ввод жилья)";
+            ws.Cells[1, 16].Value2 = "х16 (выд кредиты)";
+            ws.Cells[1, 17].Value2 = "у (цена)";
+
+            //вспомогательные данные
+            ws.Cells[1, 20].Value2 = "ссылка";
+            ws.Cells[1, 21].Value2 = "адрес";
+
+            //textBox1.Clear();
+            textBox1.AppendText("\r\n");
+
+            int i_exc = 3;
+            
+            string cb_value = comboBox1.Text.ToString();
+            string url_page = cb_value.Substring(0, cb_value.Length - 3);
+
+            int p_first_num = Convert.ToInt32(url_page),
+                p_last_num = p_first_num + 9;
+                                                           
+            for (int page_number = p_first_num; page_number <= p_last_num; page_number++)
+            {
+
+                //изменение страницы
+                //если не первая страница (уже открыта)
+                if (page_number !=p_first_num)
+                {
+                    string next_p_url = Browser.Url;
+
+                    if (page_number - 1 < 10)
+                        next_p_url = next_p_url.Substring(0, next_p_url.Length - 1);
+                    else
+                        next_p_url = next_p_url.Substring(0, next_p_url.Length - 2);
+
+                    next_p_url = next_p_url + page_number;
+                    Browser.Navigate().GoToUrl(next_p_url);
+                }
+                
+
+                //список объявлений
+                List<IWebElement> adsList;
+                adsList = Browser.FindElements(By.CssSelector(".c6e8ba5398-header--1_m0_")).ToList();
+
+                                //adsList.Count
+                                //первые 3 - рекламы - пропускаем
+                for (int i = 3; i < adsList.Count; i++)
+                {
+                    string current_url = adsList[i].GetAttribute("href").ToString();
+                    string str = "window.open('" + current_url + "','_blank');";
+                    ((IJavaScriptExecutor)Browser).ExecuteScript(str);
+                    Browser.SwitchTo().Window(Browser.WindowHandles[1].ToString());
+
+                    //задержка 5 секунд и проверка рекламного банера
+                    Thread.Sleep(5000);
+
+                    try
+                    {
+                        IWebElement add_close_button;
+                        add_close_button = Browser.FindElement(By.ClassName("a10a3f92e9--close--LxKZn"));
+                        add_close_button.Click();
+                    }
+                    catch
+                    {
+
+                    }
+                    //сбор инфы о квартире
+
+                    //год постройки
+                    List<IWebElement> year_of_building_List =
+                        Browser.FindElements(By.CssSelector(".a10a3f92e9--value--38caj")).ToList();
+
+                    //комнат, этаж, этажей в доме, тип стен
+                    List<IWebElement> general_inf_List_values =
+                        Browser.FindElements(By.CssSelector(".a10a3f92e9--value--3Ftu5")).ToList();
+                    List<IWebElement> general_inf_List_param_names =
+                        Browser.FindElements(By.CssSelector(".a10a3f92e9--name--3bt8k")).ToList();
+
+                    //общ площадь
+                    List<IWebElement> areas_List =
+                        Browser.FindElements(By.CssSelector(".a10a3f92e9--info-text--2uhvD")).ToList();
+
+
+                    //в excel
+                    if (general_inf_List_values.Count >= 6 && year_of_building_List.Count >= 1)
+                    {
+                        int j = 0;
+                        //собираем:
+
+                        //цена
+                        IWebElement price;
+                        price = Browser.FindElement(By.ClassName("a10a3f92e9--price_value--1iPpd"));
+                        string price_trimmed = price.Text.ToString();
+                        price_trimmed = price_trimmed.Substring(0, price_trimmed.Length - 2);
+                        ws.Cells[i_exc - 1, 17].Value2 = price_trimmed;
+
+                        //год постройки дома
+                        ws.Cells[i_exc - 1, 7].Value2 = year_of_building_List[0].Text.ToString();
+
+                        //комнат
+                        while (general_inf_List_param_names[j].Text.ToString() != "Количество комнат")
+                        { j++; }
+                        ws.Cells[i_exc - 1, 2].Value2 = general_inf_List_values[j].Text.ToString();
+
+                        //этаж
+                        while (general_inf_List_param_names[j].Text.ToString() != "Этаж")
+                        { j++; }
+                        ws.Cells[i_exc - 1, 3].Value2 = general_inf_List_values[j].Text.ToString();
+
+                        //этажей в доме
+                        while (general_inf_List_param_names[j].Text.ToString() != "Этажей в доме")
+                        { j++; }
+                        ws.Cells[i_exc - 1, 4].Value2 = general_inf_List_values[j].Text.ToString();
+
+                        //этажей в доме
+                        while (general_inf_List_param_names[j].Text.ToString() != "Тип дома")
+                        { j++; }
+                        try
+                        {
+                            ws.Cells[i_exc - 1, 8].Value2 = general_inf_List_values[j].Text.ToString();
+                        }
+                        catch { }
+                        //СОБИРАЕМ ОСТАЛЬНЫЕ ЭЛЕМЕНТЫ
+
+                        //общ площадь
+                        string area = areas_List[0].Text.ToString();
+                        area = area.Substring(0, area.Length - 3);
+                        ws.Cells[i_exc - 1, 1] = area;
+
+
+                        //ссылка
+                        ws.Cells[i_exc - 1, 20].Value2 = current_url;
+
+                        //адрес дома
+                        IWebElement adress;
+                        adress = Browser.FindElement(By.ClassName("a10a3f92e9--address--140Ec"));
+                        string adress_trimmed;
+                        adress_trimmed = adress.Text.ToString();
+                        adress_trimmed = adress_trimmed.Substring(0, adress_trimmed.Length - 8);
+                        ws.Cells[i_exc - 1, 21] = adress_trimmed;
+
+                        i_exc++;
+                    }
+
+                    //добавление в текстовый элемент
+                    textBox1.AppendText(current_url + "\r\n");
+
+
+                    //закрытие вкладки с текущим объявлением
+                    Browser.Close();
+                    Browser.SwitchTo().Window(Browser.WindowHandles[0].ToString());
+                    //string str2 = "window.location.href='" + url2 + "';";
+                }
+
+
+
+            }
+
+            string new_path = @"C:\курсовая\парсер недвижимость курсач\данные " +
+                DateTime.Now.ToString().Replace('/', '-').Replace(':', '-') + ".xlsx";
+
+            //wb.Save();
+            wb.SaveAs(new_path);
+            wb.Close();
+
+            Browser.Quit();
+            Browser = null;
+            comboBox1.Enabled = true;
+        }
+        
     }
 }
